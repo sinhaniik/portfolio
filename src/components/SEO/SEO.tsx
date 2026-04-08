b896d03442cd199eb4c387d59cb84742e519d0ce
Nikhil Sinha <nikhilsinha198@gmail.com>
2026-04-08 21:06:08 +0530

    add header icon

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -8,25 +8,25 @@ interface SEOProps {
   type?: string;
 }
 
-export const SEO = ({ title, description, url, image, type = 'website' }: SEOProps) => {
+export const SEO = ({ description, url, image, type = 'website' }: SEOProps) => {
   const siteName = "Nikhil | SDE & Infrastructure";
   return (
     <Helmet>
       {/* Standard metadata tags */}
-      <title>{title === 'Home' ? siteName : `${title} | ${siteName}`}</title>
+      <title>{siteName}</title>
       <meta name="description" content={description} />
       
       {/* Open Graph / Facebook */}
       <meta property="og:type" content={type} />
       {url && <meta property="og:url" content={url} />}
-      <meta property="og:title" content={title === 'Home' ? siteName : `${title} | ${siteName}`} />
+      <meta property="og:title" content={siteName} />
       <meta property="og:description" content={description} />
       {image && <meta property="og:image" content={image} />}
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       {url && <meta property="twitter:url" content={url} />}
-      <meta name="twitter:title" content={title === 'Home' ? siteName : `${title} | ${siteName}`} />
+      <meta name="twitter:title" content={siteName} />
       <meta name="twitter:description" content={description} />
       {image && <meta name="twitter:image" content={image} />}
     </Helmet>
